b5a46c3e6a33bfbf180ee6bd67cd74eea4cfc6ee
rahularya198920 <36296566+rahularya198920@users.noreply.github.com>
2018-07-10 00:20:03 +0200

    added login fields
    added login

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 from flask import Flask,render_template,request
+from flaskext.mysql import MySQL
 
 import json
 app= Flask(__name__)
 
-
+mysql = MySQL()
+#app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+ 
 @app.route("/")
 def main():
     return render_template('index.html')
@@ -23,6 +31,18 @@ def signUp():
         return json.dumps({'html':'<span> All fields good !!</span>'})
     else:
         return json.dumps({'html':'<span> Enter the required field</span>'})
+    
+@app.route("/Authenticate")
+def Authenticate():
+    username = request.args.get('UserName')
+    password = request.args.get('Password')
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
+    data = cursor.fetchone()
+    if data is None:
+     return "Username or Password is wrong"
+    else:
+     return "Logged in successfully"
 
 
 
